0655ed445cc731dc727612216a3af0728911af3a
amank2215 <amankumarkv1sec30@gmail.com>
2025-12-29 07:56:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 $(function(){
 	$("#about-me").typed({
-		strings: ["Student at IIIT-NR.","Co-ordinator at E-Cell IIIT-NAYA RAIPUR","Sales and Marketing Executive at HostelDhaba","Design Team Member at Technovate","Open Source Enthusiast.","Interested in Machine Learning and AI.","Loves to Code <&#x2764>."],
+		strings: ["I am an Alumnus of IIIT-NR and a State Merit Scholar.", "I lead Global SaaS Implementations for Enterprise Giants.", "I have managed Tech Portfolios exceeding $10 Million.", "I am an AI Architect and Machine Learning Specialist.", "I am a Winner of multiple National-level Tech Summits.", "I am passionate about building the Future through Innovation.", "I was a Vice President and Sports Leader at IIIT-NR <&#x2764>."],
 		typeSpeed: 30,
 		loop: true,
 	});
